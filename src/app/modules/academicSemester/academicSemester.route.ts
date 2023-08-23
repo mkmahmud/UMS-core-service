72a0f602d9730888ddd10a0789e36aae6b4e8cd0
mkmahmud <mahmudulhasanmk434@gmail.com>
2023-08-23 13:48:11 +0600

    [SETUP] Authorization in Academic Semester

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { academicSemesterController } from './academicSemester.controller';
 import { academicSemesterValidation } from './academicSemester.validation';
@@ -11,21 +13,27 @@ router.get('/:id', academicSemesterController.getDataById);
 // Create Academic semester
 router.post(
   '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(academicSemesterValidation.create),
   academicSemesterController.createAcademicSemester
 );
 
 // Delete Semester Data by Id
-router.delete('/', academicSemesterController.deleteSemesterById);
+router.delete(
+  '/',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  academicSemesterController.deleteSemesterById
+);
 
 // Update Academic semester
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(academicSemesterValidation.update),
   academicSemesterController.updateSemesterById
 );
 
 // Get All Semester Data
-router.delete('/:id', academicSemesterController.deleteSemesterById);
+router.get('/:id', academicSemesterController.getAllSemester);
 
 export const academicSemesterRoutes = router;
